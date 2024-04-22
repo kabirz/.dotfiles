280568b9886e196fb7b49d2e6f9635b2507c2533
kabirz <jxwazxzhp@126.com>
2024-04-22 10:41:57 +0800

    update zellij cpu info
    Signed-off-by: kabirz <jxwazxzhp@126.com>

--- zellij/scripts/cpu_info.sh
+++ zellij/scripts/cpu_info.sh
@@ -2,26 +2,17 @@
 
 export LC_ALL=en_US.UTF-8
 
-normalize_percent_len() {
-  max_len=3
-  percent_len=${#1}
-  let diff_len=$max_len-$percent_len
-  let left_spaces=($diff_len+1)/2
-  let right_spaces=($diff_len)/2
-  printf "CPU %${left_spaces}s%s%${right_spaces}s\n" "" $1 ""
-}
 case $(uname -s) in
   Linux)
-    percent=$(LC_NUMERIC=en_US.UTF-8 top -bn2 -d 0.01 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
-    normalize_percent_len $percent
+    percent=$(LC_NUMERIC=en_US.UTF-8 top -bn2 -d 0.01 | grep "Cpu(s)" | tail -1 | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+    printf "CPU %4s%%" $percent
     ;;
 
   Darwin)
     cpuvalue=$(ps -A -o %cpu | awk -F. '{s+=$1} END {print s}')
     cpucores=$(sysctl -n hw.logicalcpu)
     cpuusage=$(( cpuvalue / cpucores ))
-    percent="$cpuusage%"
-    normalize_percent_len $percent
+    printf "CPU %4s%%" $cpuusage
     ;;
 
   *)
